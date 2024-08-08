9a611e3b61418fdf76500e72a8188441e0437ed3
mohamed hassan <mohamed.hassan@thirdwayv.com>
2024-08-08 13:03:01 +0300

    test

--- commit.bat
+++ commit.bat
@@ -1,15 +1,3 @@
 echo off
 call python readme_edit.py
 if %ERRORLEVEL% NEQ 0 exit 1
-
-call git commit .\README.md -m " new commit"
-if %ERRORLEVEL% NEQ 0 exit 1
-
-call git push
-if %ERRORLEVEL% NEQ 0 (
-echo fail
-pause
-) ELSE (
-echo success
-pause
-)
